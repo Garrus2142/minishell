289ce0ea645612fb799724f040ef78582f1f5a12
Timothee HUGO <thugo@e2r10p22.42.fr>
2017-10-27 03:32:22 +0200

    Support arg -PL

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: thugo <thugo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/25 02:08:55 by thugo             #+#    #+#             */
-/*   Updated: 2017/10/25 02:14:45 by thugo            ###   ########.fr       */
+/*   Updated: 2017/10/27 02:34:56 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,21 @@
 int	builtin_pwd(t_data *data, int argc, char **argv)
 {
 	char	cwd[MAXPATHLEN];
+	char	*options;
 
-	(void)data;
-	(void)argc;
-	(void)argv;
-	if (!getcwd(cwd, MAXPATHLEN))
+	if (ft_getopt(argc, argv, "LP", &options) == -1)
+	{
+		ft_dprintf(2, "pwd: bad option: -%c\n", *options);
 		return (EXIT_FAILURE);
-	ft_putendl(cwd);
+	}
+	if (!ft_strchr(options, 'P') && env_get(data, "PWD"))
+		ft_putendl(env_get(data, "PWD"));
+	else
+	{
+		if (!getcwd(cwd, MAXPATHLEN))
+			return (EXIT_FAILURE);
+		ft_putendl(cwd);
+	}
+	free(options);
 	return (EXIT_SUCCESS);
 }
