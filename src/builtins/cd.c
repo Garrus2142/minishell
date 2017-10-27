9a49dee48c7c72e3dbef4d5d333b726bec383e0d
Timothee HUGO <thugo@e2r10p22.42.fr>
2017-10-27 04:16:26 +0200

    WIP: Support logical path

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: thugo <thugo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/25 02:43:07 by thugo             #+#    #+#             */
-/*   Updated: 2017/10/25 20:03:50 by thugo            ###   ########.fr       */
+/*   Updated: 2017/10/27 04:15:55 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <unistd.h>
 #include "minishell.h"
 
-static int	change_dir(char *path)
+static int	can_change(char *path)
 {
 	char	stats;
 
@@ -28,17 +28,28 @@ static int	change_dir(char *path)
 			ft_dprintf(2, "cd: not a directory: %s\n", path);
 		else if (!(stats & STATS_EXEC))
 			ft_dprintf(2, "cd: permission denied: %s\n", path);
-		return (1);
+		return (0);
 	}
-	chdir(path);
-	return (0);
+	return (1);
 }
+
+/*static char	logical_path(char *path)
+{
+	
+}*/
 
 int			builtin_cd(t_data *data, int argc, char **argv)
 {
 	char	*path;
+	char	*options;
+	int		index;
 
-	if (argc == 1)
+	if ((index = ft_getopt(argc, argv, "LP", &options)) == -1)
+	{
+		ft_dprintf(2, "cd: bad option: -%c\n", *options);
+		return (EXIT_FAILURE);
+	}
+	if (index >= argc)
 	{
 		if (!(path = env_get(data, "HOME")))
 		{
@@ -47,6 +58,10 @@ int			builtin_cd(t_data *data, int argc, char **argv)
 		}
 	}
 	else
-		path = argv[1];
-	return (change_dir(path));
+		path = argv[index];
+	if (!can_change(path))
+		return (1);
+	if (ft_strchr(options, 'P'))
+		chdir(path);
+	return (0);
 }
