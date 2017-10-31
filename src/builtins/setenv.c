a42611a51484d02443b43b2f855f7ba01f5af467
Timothee HUGO <thugo@e2r10p22.42.fr>
2017-11-01 00:56:59 +0100

    Support set with no value

--- src/builtins/setenv.c
+++ src/builtins/setenv.c
@@ -6,26 +6,26 @@
 /*   By: thugo <thugo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/31 22:59:08 by thugo             #+#    #+#             */
-/*   Updated: 2017/10/31 23:58:46 by thugo            ###   ########.fr       */
+/*   Updated: 2017/11/01 00:10:41 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "minishell.h"
 
-static int	check_errors(int argc, char **argv)
+static int	check_errors(char *name)
 {
 	int	i;
 
-	if (!ft_isalpha(argv[1][0]))
+	if (!ft_isalpha(name[0]))
 	{
 		ft_dprintf(2, "setenv: Variable name must begin with a letter.\n");
 		return (1);
 	}
 	i = 0;
-	while (argv[1][i])
+	while (name[i])
 	{
-		if (!ft_isalnum(argv[1][i++]))
+		if (!ft_isalnum(name[i++]))
 		{
 			ft_dprintf(2,
 			"setenv: Variable name must contain alphanumeric characters.\n");
@@ -40,13 +40,13 @@ int			builtin_setenv(t_data *data, int argc, char **argv)
 	int	err;
 
 	err = 0;
-	if (argc < 3)
+	if (argc < 2)
 	{
 		ft_dprintf(2, "usage: setenv [name] [value]\n");
 		return (1);
 	}
-	if (check_errors(argc, argv))
+	if (check_errors(argv[1]))
 		return (1);
-	env_set(data, argv[1], argv[2]);
+	env_set(data, argv[1], argc > 2 ? argv[2] : "");
 	return (0);
 }
